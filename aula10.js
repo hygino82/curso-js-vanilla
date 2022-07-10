a3973acf2f262bfc1df498ab2b44bc8453d622ef
Adroaldo <hygino@msn.com>
2022-07-10 15:03:42 -0300

    Arrays parte 1

---
+++ aula10.js
@@ -0,0 +1,16 @@
+//formal
+let myArray = new Array('Dilma', 'Gorete', 51);
+//literal
+let myArr2 = ['Banana', 'Torresmo', 15];
+console.log(myArr2);
+//associativo
+let myArr3 = {
+    nome: 'Jupira',
+    solteira: true,
+    idade: 69
+}
+
+myArr3['trabalha'] = true;
+console.log(myArr3);
+console.log(myArr3['nome']);
+console.log(myArr2.constructor);
