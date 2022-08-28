abcca2fdb59f3a6058a1ee72f4870d81e276e557
Adroaldo <hygino@msn.com>
2022-08-28 20:41:23 -0300

    Ajax PHP post

--- aula30/aula30.js
+++ aula30/aula30.js
@@ -5,14 +5,20 @@
 
     //send Ajax form
     function sendForm(event) {
+        event.preventDefault();
+
         let ajax = new XMLHttpRequest();
+        let params = 'name=' + doc.querySelector('#name').value;
         ajax.open('POST', 'controller.php');
+        ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         ajax.onreadystatechange = function () {
-            console.log(ajax.status);
-            console.log(ajax.readyState);
-            console.log(ajax.responseText);
+            if (ajax.status === 200 && ajax.readyState === 4) {
+                //console.log(JSON.parse(ajax.responseText));
+                let json = JSON.parse(ajax.responseText);
+                console.log(json.name + ' - ' + json.age + ' - ' + json.city);
+            }
         };
-        ajax.send();
+        ajax.send(params);
     }
 
     form.addEventListener('submit', sendForm, false);
--- aula30/controller.php
+++ aula30/controller.php
@@ -1,2 +1,8 @@
 <?php
-echo "Hello World!";
+$name=filter_input(INPUT_POST,'name',FILTER_SANITIZE_SPECIAL_CHARS);
+$arr=[
+    'name'=>$name,
+    'age'=>18,
+    'city'=>'Coronel Vivida'
+];
+echo json_encode($arr);
